3dfcba41ca1aab3e114e81a1cf389004952e4068
freroxx <yikni2829@gmail.com>
2025-12-09 08:59:56 +0100

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,6 +1,20 @@
+// components/ThemeProvider.tsx
+"use client"
+
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark" // Gardez dark comme thème par défaut
+      enableSystem={true}
+      enableColorScheme={true}
+      disableTransitionOnChange={false} // IMPORTANT : permet les transitions
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
