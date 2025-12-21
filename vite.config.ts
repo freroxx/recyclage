3022da37c157af0c47089f6a4c6da11808caf328
freroxx <yikni2829@gmail.com>
2025-12-21 17:28:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import prerender from "vite-plugin-prerender"; // Added ts
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -21,11 +22,17 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       mode === "development" ? componentTagger() : undefined,
+      mode === "production"
+        ? prerender({
+            routes: ["/", "/support", "resources", "/guide", "/activities", "/videos", "/posters", "/project", "/contact"], // pages for render (yeah i'm a genious, I know)
+            rendererOptions: { headless: true },
+          })
+        : undefined,
     ].filter(Boolean),
     build: {
-      target: "es2018", // ✅ plus sûr que esnext
+      target: "es2018",
       outDir: "dist",
-      sourcemap: false, // ✅ prod clean
+      sourcemap: false,
       rollupOptions: {
         output: {
           manualChunks: {
