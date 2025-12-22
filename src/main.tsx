b9dfa8991d9218a5ac2e700a3d438e66c81edcef
freroxx <yikni2829@gmail.com>
2025-12-22 20:08:42 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-// main.tsx
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
+
 if (container) {
   const root = createRoot(container);
-  root.render(<App skipOnboardingNoJS={true} />); // <-- pass prop here
+  // Pass `skipOnboardingNoJS` for Googlebot / JS-off
+  root.render(<App skipOnboardingNoJS={true} />);
 }
