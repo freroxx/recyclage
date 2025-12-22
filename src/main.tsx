0b43a0e433f70798ced547f55fd13eb0735b3272
freroxx <yikni2829@gmail.com>
2025-12-22 20:49:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import App from "./App.tsx";
 import "./index.css";
 
 const rootEl = document.getElementById("root")!;
-const skipOnboardingAttr = rootEl.dataset.skipOnboarding === "true";
+
+// Détection simple de bots Google / Aperçu des annonces ayeh ayeh
+const isBot = /Googlebot|AdsBot-Google|Mediapartners-Google/i.test(navigator.userAgent);
 
 createRoot(rootEl).render(
-  <App skipOnboardingNoJS={skipOnboardingAttr} />
+  <App skipOnboardingNoJS={isBot} />
 );
