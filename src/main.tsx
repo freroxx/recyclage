54c8880999f652fb01eb45bd29b53650b4012047
freroxx <yikni2829@gmail.com>
2025-12-22 19:58:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Récupère le root
 const container = document.getElementById("root");
 if (container) {
   const root = createRoot(container);
-  // Passe le prop pour skip l'onboarding quand JS est actif
+  // skipOnboardingNoJS = true signifie qu’on ignore l’onboarding si JS est actif
   root.render(<App skipOnboardingNoJS={true} />);
 }
