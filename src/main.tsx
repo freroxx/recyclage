f188921c7e08e9c891551a67cf29b474e6741c10
freroxx <yikni2829@gmail.com>
2025-12-22 20:11:29 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
-
 if (container) {
   const root = createRoot(container);
-  // Pass `skipOnboardingNoJS` for Googlebot / JS-off
   root.render(<App skipOnboardingNoJS={true} />);
 }
